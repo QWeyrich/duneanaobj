d4693c2d76609f353b97f3745a19fd273e21d8d5
QWeyrich <q.weyrich@gmail.com>
2025-08-13 13:49:19 -0400

    Time is now stored at the track level instead of the interaction level

--- duneanaobj/StandardRecord/SRTMS.h
+++ duneanaobj/StandardRecord/SRTMS.h
@@ -14,7 +14,6 @@ namespace caf
     public:
       std::vector<caf::SRTrack> tracks;
       std::size_t               ntracks  = 0;
-      float                     time = NaN; // Added time of interaction
   };
 
   /// The information needed to uniquely identify a TMS reco object
